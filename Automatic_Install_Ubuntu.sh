d953590fe877121f693d8c6aa6c25671c2f1c67b
LaTex <32957066+junjiecjj@users.noreply.github.com>
2022-01-06 12:41:18 +0800

    Update Automatic_Install_Ubuntu.sh

--- Automatic_Install_Ubuntu.sh
+++ Automatic_Install_Ubuntu.sh
@@ -121,6 +121,14 @@ echo ${PASSWD} | sudo -S   mkfontscale  #生成核心字体信息
 echo ${PASSWD} | sudo -S  mkfontdir
 echo ${PASSWD} | sudo -S  fc-cache -fv
 cd
+# 或者
+wget -O /tmp/YaHei.Consolas.1.12.zip https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/uigroupcode/YaHei.Consolas.1.12.zip
+unzip ~/tmp/YaHei.Consolas.1.12.zip
+sudo mkdir -p /usr/share/fonts/consolas
+sudo mv YaHei.Consolas.1.12.ttf /usr/share/fonts/consolas/
+sudo chmod 644 /usr/share/fonts/consolas/YaHei.Consolas.1.12.ttf
+cd /usr/share/fonts/consolas
+sudo mkfontscale && sudo mkfontdir && sudo fc-cache -fv
 
 echo -e "${GREEN_BLACK}************************* 6、安装字体 Consolas 等 *************************${COLOR_RESET} \n"
 #解压压缩包
